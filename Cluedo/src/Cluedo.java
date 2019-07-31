8636184cf3c0cf76455a372fc898d76ea1e50e38
Fergus Currie <fergus.currie@gmail.com>
2019-08-01 11:57:32 +1200

    umpl

--- Cluedo/src/Cluedo.java
+++ Cluedo/src/Cluedo.java
@@ -1,4 +1,73 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.1.4597.b7ac3a910 modeling language!*/
 
-public class Cluedo {
-	//main class for cluedo proejct 
+
+
+// line 47 "model.ump"
+// line 135 "model.ump"
+public class Cluedo
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Cluedo Associations
+  private Envelope envelope;
+  private Board board;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Cluedo(Envelope aEnvelope, Board aBoard)
+  {
+    if (aEnvelope == null || aEnvelope.getCluedo() != null)
+    {
+      throw new RuntimeException("Unable to create Cluedo due to aEnvelope. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    }
+    envelope = aEnvelope;
+    if (aBoard == null || aBoard.getCluedo() != null)
+    {
+      throw new RuntimeException("Unable to create Cluedo due to aBoard. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    }
+    board = aBoard;
+  }
+
+  public Cluedo(RoomCard aRoomCardForEnvelope, WeaponCard aWeaponCardForEnvelope, CharacterCard aCharacterCardForEnvelope)
+  {
+    envelope = new Envelope(aRoomCardForEnvelope, aWeaponCardForEnvelope, aCharacterCardForEnvelope, this);
+    board = new Board(this);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+  /* Code from template association_GetOne */
+  public Envelope getEnvelope()
+  {
+    return envelope;
+  }
+  /* Code from template association_GetOne */
+  public Board getBoard()
+  {
+    return board;
+  }
+
+  public void delete()
+  {
+    Envelope existingEnvelope = envelope;
+    envelope = null;
+    if (existingEnvelope != null)
+    {
+      existingEnvelope.delete();
+    }
+    Board existingBoard = board;
+    board = null;
+    if (existingBoard != null)
+    {
+      existingBoard.delete();
+    }
+  }
+
 }
