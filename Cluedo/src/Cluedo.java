1fbb8813ac9f5cb0a3febe19d487b639bd12cca0
Fergus Currie <fergus.currie@gmail.com>
2019-08-01 12:49:42 +1200

    cluedo changes

--- Cluedo/src/Cluedo.java
+++ Cluedo/src/Cluedo.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.29.1.4597.b7ac3a910 modeling language!*/
 
@@ -15,59 +19,115 @@ public class Cluedo
   //Cluedo Associations
   private Envelope envelope;
   private Board board;
+  
+  //
 
   //------------------------
   // CONSTRUCTOR
   //------------------------
-
-  public Cluedo(Envelope aEnvelope, Board aBoard)
-  {
-    if (aEnvelope == null || aEnvelope.getCluedo() != null)
-    {
-      throw new RuntimeException("Unable to create Cluedo due to aEnvelope. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    envelope = aEnvelope;
-    if (aBoard == null || aBoard.getCluedo() != null)
-    {
-      throw new RuntimeException("Unable to create Cluedo due to aBoard. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    board = aBoard;
+  /**
+   * Constructor for Cluedo. Runs the main game mechanics. 
+   */
+  public Cluedo() {
+	  //Get num players and create players 
+	  int numPlayers = getNumPlayers();
+	  HashMap<Integer,Player> players = new HashMap<Integer,Player>();
+	  //choose characters 
+	  chooseCharacters();
+	  //New Board
+	 // Board board = new Board(numPlayers);
+	  
+	  //Turns : dice -> move -> suggestion(in room) or accusation
+  }
+  
+  public ArrayList<String> chooseCharacters() {
+	  return null;
+  }
+  
+  /**
+   * Method to get the number of players.
+   * Has checking to make sure they give good inputs 
+   * @return int
+   */
+  public int getNumPlayers() {
+	  boolean foundNum = false;
+	  int numPlayers = -1;
+	  while(!foundNum) {
+		  String strNumPlayers = askPlayer("How many people are playing? ");
+		  int numErrorMsg = 0;
+		  try {
+			 foundNum = true;
+		     numPlayers = Integer.parseInt(strNumPlayers);
+		  }
+		  catch (NumberFormatException e) {
+		     foundNum = false;
+		     numPlayers = -1;
+		     System.out.println("Input error, please enter 2 , 3 or 4");
+		     numErrorMsg++;
+		  }
+		  if(numPlayers < 2 || numPlayers > 4) {
+			  if(numErrorMsg == 0) {
+				  System.out.println("Input error, please enter 2 , 3 or 4");
+			  }
+			  numPlayers = -1;
+			  foundNum = false;
+		  }
+	  }
+	  return numPlayers;
   }
 
-  public Cluedo(RoomCard aRoomCardForEnvelope, WeaponCard aWeaponCardForEnvelope, CharacterCard aCharacterCardForEnvelope)
-  {
-    envelope = new Envelope(aRoomCardForEnvelope, aWeaponCardForEnvelope, aCharacterCardForEnvelope, this);
-    board = new Board(this);
-  }
 
   //------------------------
   // INTERFACE
   //------------------------
-  /* Code from template association_GetOne */
-  public Envelope getEnvelope()
-  {
+  /**
+   * Getter for envelope 
+   * @return Envelope
+   */
+  public Envelope getEnvelope() {
     return envelope;
   }
-  /* Code from template association_GetOne */
-  public Board getBoard()
-  {
+  /**
+   * getter for Board 
+   * @return Board 
+   */
+  public Board getBoard() {
     return board;
   }
-
-  public void delete()
-  {
-    Envelope existingEnvelope = envelope;
-    envelope = null;
-    if (existingEnvelope != null)
-    {
-      existingEnvelope.delete();
-    }
-    Board existingBoard = board;
-    board = null;
-    if (existingBoard != null)
-    {
-      existingBoard.delete();
-    }
+  /**
+   * Asks the player for input 
+   * @param args
+   */
+  public String askPlayer(String s) {
+	  System.out.print(s);
+	  Scanner sc = new Scanner(System.in);
+	  String ans = sc.next();
+	  return ans;
   }
+  
+  
+  public static void main(String args[]) {
+	  new Cluedo();
+  }
+  
+  
+  
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
