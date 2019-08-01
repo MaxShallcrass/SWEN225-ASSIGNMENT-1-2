bd48470da0320442645aafcc8a989e6ac1454768
macpa <macpa@192.168.20.9>
2019-08-01 15:10:56 +1200

    update

--- Cluedo/src/Board.java
+++ Cluedo/src/Board.java
@@ -1,103 +1,141 @@
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Board
-{
+public class Board {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+	// ------------------------
+	// MEMBER VARIABLES
+	// ------------------------
 
-  //Board Associations
-  private Cluedo cluedo;
-  private List<Player> players;
-  private List<Cell> cells;
-  private Cell[][] board;
+	// Board Associations
+	private Cluedo cluedo;
+	private List<Player> players;
+	private List<Cell> cells;
+	private Cell[][] board;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+	// ------------------------
+	// CONSTRUCTOR
+	// ------------------------
+	/*
+	 * Constructs the board for the game.
+	 * Loads the board from map layout.txt
+	 * Adds players to the board
+	 */
+	public Board(List<Player> realPlayers, List<Player> nonPlayer) {
+		// Placing all the cells on the board
+		board = new Cell[24][25];
+		try {
+			Scanner sc = new Scanner(new File("map layout.txt"));
 
-  public Board(List<Player> realPlayers, List<Player> nonPlayer){
-	  //Placing all the cells on the board
-	  board= new Cell[24][25];
-	  try {
-		Scanner sc= new Scanner(new File("map layout.txt"));
-		
-		int x=0, y=0;
-		while(sc.hasNext()) { //scans through board txt file
-		String token=sc.next();
-		Location loc=new Location(x++, y++);
-		if(token.length()>1) { // A door
-			DoorCell dc = new DoorCell(loc, token);
-			board[x][y]=dc;
-			
-		}else { //floor tile
-			FloorCell fc= new FloorCell(loc, token.charAt(0));
-			board[x][y]= fc;	
+			int x = 0, y = 0;
+			while (sc.hasNext()) { // scans through board txt file
+				String token = sc.next();
+				Location loc = new Location(x++, y++);
+				if (token.length() > 1) { // A door
+					DoorCell dc = new DoorCell(loc, token);
+					board[x][y] = dc;
+
+				} else { // floor tile
+					FloorCell fc = new FloorCell(loc, token.charAt(0));
+					board[x][y] = fc;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error loading board");
 		}
+		List<Location> playerLocs = new ArrayList<Location>(); // Starter locations for players
+		playerLocs.add(new Location(0, 9));
+		playerLocs.add(new Location(0, 14));
+		playerLocs.add(new Location(23, 6));
+		playerLocs.add(new Location(23, 19));
+		playerLocs.add(new Location(7, 24));
+		playerLocs.add(new Location(0, 17));
+
+		// Placing the players on the board
+		for (int i = 0; i < realPlayers.size(); i++) {
+			board[playerLocs.get(i).getX()][playerLocs.get(i).getY()].setPlayer(realPlayers.get(i));
+			realPlayers.get(i).setLocation(playerLocs.get(i));
 		}
-	  } catch (FileNotFoundException e) {
-		System.out.println("Error loading board");
+		for (int i = realPlayers.size(); i < realPlayers.size() + nonPlayer.size(); i++) {
+			board[playerLocs.get(i).getX()][playerLocs.get(i).getY()].setPlayer(nonPlayer.get(i));
+			nonPlayer.get(i).setLocation(playerLocs.get(i));
+		}
 	}
-	  List<Location> playerLocs= new ArrayList<Location>(); //Starter locations for players
-	  playerLocs.add(new Location(0, 9)); playerLocs.add(new Location(0, 14));
-	  playerLocs.add(new Location(23, 6)); playerLocs.add(new Location(23, 19));
-	  playerLocs.add(new Location(7, 24)); playerLocs.add(new Location(0, 17));
-	  
-	  
-	  //Placing the players on the board
-	  for(int i=0; i<realPlayers.size(); i++) {
-		  board[playerLocs.get(i).getX()][playerLocs.get(i).getY()].setPlayer(realPlayers.get(i));
-		  realPlayers.get(i).setLocation(playerLocs.get(i));
-	  }
-	 for(int i= realPlayers.size();i< realPlayers.size()+nonPlayer.size(); i++) {
-		 board[playerLocs.get(i).getX()][playerLocs.get(i).getY()].setPlayer(nonPlayer.get(i));
-		  nonPlayer.get(i).setLocation(playerLocs.get(i));
-	 } 
-  }
-  
-  /*
-   * displays the board to text output
-   */
-  public void displayBoard() {
-	  for(int x=0; x<24; x++) {
-		  for(int y=0; y<25; y++) {
-			  System.out.print(board[x][y].toString());
-		  }
-		  System.out.println();
-	  }
-  }
-  /*
-   * Moves a player one place on the board
-   * up, down, left, right
-   */
-  public void movePlayer() {
-	  
-  }
-  
-  /*
-   * Checks if its valid to move a player in the direction
-   */
-  private boolean isValidMove() {
-	  return false;
-  }
-  
-  
-  /*
-   * Returns a cell at the Location, x,y
-   */
-  public Cell getCellAt(Location loc) {
-	  
-	  return null;
-  }
 
+	/*
+	 * displays the board to text output
+	 */
+	public void displayBoard() {
+		for (int x = 0; x < 24; x++) {
+			for (int y = 0; y < 25; y++) {
+				System.out.print(board[x][y].toString());
+			}
+			System.out.println();
+		}
+	}
 
+	/*
+	 * Moves a player one place on the board up, down, left, right
+	 */
+	public int movePlayer(Location locAt, String direction, int movesLeft) {
+		int x=locAt.getX();
+		int y=locAt.getY();
+		if(direction.compareToIgnoreCase("w")==0)
+			y-=1;
+		if(direction.compareToIgnoreCase("a")==0)
+			x-=1;
+		if(direction.compareToIgnoreCase("s")==0)
+			y+=1;
+		if(direction.compareToIgnoreCase("d")==0)
+			x+=1;
+		Location locTo = new Location(x, y);
+		isValidMove(locAt, locTo);
+		
+		
+		return movesLeft;
+	}
 
+	/*
+	 * Checks if its valid to move a player in the direction
+	 */
+	private boolean isValidMove(Location at, Location to) {
+		int x=to.getX();
+		int y=to.getY();
+		// Checks error bounds
+		if(x<0 || x>=24 || y<0 || y>=24) { 
+			System.out.println("Invalid move - Out of bounds: Retry again");
+			return false;
+		}
+		//checks that player hasnt moved to that cell this turn
+		List<Location> prevsLocs = getPlayerAt(at).getLocations();
+		for(Location loc: prevsLocs) {
+			if(loc.equals(to)) {
+				System.out.println("Invalid move - Already visited tile this turn: Retry again");
+				return false;
+			}
+				
+		}
+		
+		
+		return false;
+	}
+	
+	/*
+	 * Gets player at location on the board
+	 */
+	private Player getPlayerAt(Location loc) {
+		Player p = board[loc.getX()][loc.getY()].getPlayer();
+		return p;
+	}
 
+	/*
+	 * Returns a cell at the Location, x,y
+	 */
+	public Cell getCellAt(Location loc) {
 
+		return null;
+	}
 
 }
--- Cluedo/src/Cell.java
+++ Cluedo/src/Cell.java
@@ -1,36 +1,42 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.29.1.4597.b7ac3a910 modeling language!*/
 
-
-
 // line 23 "model.ump"
 // line 106 "model.ump"
-public class Cell
-{
-private boolean hasPlayer;
-private Location loc;
-private Player player;
+public class Cell {
+	private boolean hasPlayer;
+	private Location loc;
+	private Player player;
 
-Cell(Location loc){
-	this.loc=loc;
-	hasPlayer=false;
-}
+	Cell(Location loc) {
+		this.loc = loc;
+		hasPlayer = false;
+	}
 
-public void setPlayer(Player p) {
-	this.player=p;
-	hasPlayer=true;	
-}
-public void removePlayer() {
-	player=null;
-	hasPlayer=false;
-}
+	public void setPlayer(Player p) {
+		this.player = p;
+		hasPlayer = true;
+	}
 
-public boolean hasPlayer() {
-	return hasPlayer;
-}
+	public void removePlayer() {
+		player = null;
+		hasPlayer = false;
+	}
 
-public String toString() {
-	return player.toString();
-}
+	public boolean hasPlayer() {
+		return hasPlayer;
+	}
+	
+	public Player getPlayer() {
+		if(!hasPlayer)
+			throw new RuntimeException("Error: Cell that does not have a player - getPlayer");
+		return player;
+	}
 
+	public String toString() {
+		if (!hasPlayer)
+			throw new RuntimeException("Error: Cell that does not have a player - toString - could be an inheiritance issue");
+
+		return player.toString();
+	}
 }
